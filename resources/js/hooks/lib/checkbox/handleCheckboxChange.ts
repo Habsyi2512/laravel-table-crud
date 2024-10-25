078f9de01545896a04086215e830321eda288308
Habsyi2512 <githabsyi@gmail.com>
2024-10-25 07:56:46 +0700

    "Updated handleCheckboxChange function to accept two parameters and removed unused import"

--- resources/js/hooks/lib/checkbox/handleCheckboxChange.ts
+++ resources/js/hooks/lib/checkbox/handleCheckboxChange.ts
@@ -1,16 +1,18 @@
-// import { useCallback } from "react";
+import { ListSelectedRowsProps } from "@/interface/inputProps";
+import { InputItem } from "@/interface/props";
+import { useCallback } from "react";
 
-// export const handleCheckboxChange = (kecamatan:Kecamatan) => {
-//     setSelectedRows((prevSelectedRows) => {
-//         const isSelected = prevSelectedRows.tabelKecamatanRows.some((row) => row.id === kecamatan.id);
-//         const updatedRows = isSelected ? prevSelectedRows.tabelKecamatanRows.filter((row) => row.id !== kecamatan.id) : [...prevSelectedRows.tabelKecamatanRows, kecamatan];
-//         return {
-//             ...prevSelectedRows,
-//             tabelKecamatanRows: updatedRows,
-//             length: {
-//                 ...prevSelectedRows.length,
-//                 kecamatan: updatedRows.length,
-//             },
-//         };
-//     });
-// };
+export const handleCheckboxChange = (item:InputItem, setSelectedRows:React.Dispatch<React.SetStateAction<ListSelectedRowsProps>>) => {
+    setSelectedRows((prevSelectedRows) => {
+        const isSelected = prevSelectedRows.tabelKecamatanRows.some((row) => row.id === item.id);
+        const updatedRows = isSelected ? prevSelectedRows.tabelKecamatanRows.filter((row) => row.id !== item.id) : [...prevSelectedRows.tabelKecamatanRows, item];
+        return {
+            ...prevSelectedRows,
+            tabelKecamatanRows: updatedRows,
+            length: {
+                ...prevSelectedRows.length,
+                kecamatan: updatedRows.length,
+            },
+        };
+    });
+};
